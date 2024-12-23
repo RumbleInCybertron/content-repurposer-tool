d7d73e27f679ce5da53c2ffca244f93485d7822a
Derek <RumbleInCybertron@users.noreply.github.com>
2024-12-23 10:18:46 +0900

    install jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(module-to-transform)/)',
+  ],
+};
