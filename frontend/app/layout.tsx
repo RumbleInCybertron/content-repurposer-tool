cd2fb59ed9186e4f5f2f88e49995ed1b5949aa1b
Derek <RumbleInCybertron@users.noreply.github.com>
2024-12-26 23:20:44 +0900

    + sess provider to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import "./globals.css";
 import NavBar from '../components/NavBar';
 import { links } from '../components/NavLinkConfig';
+import { SessionProvider } from 'next-auth/react';
 
 export default function RootLayout({
   children,
@@ -9,10 +12,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"antialiased"}>
-        <NavBar links={links} />
-        <main className="p-6">{children}</main>
-      </body>
-    </html>
+        <body className={"antialiased"}>
+          <NavBar links={links} />
+          <SessionProvider>
+            {children}
+          </SessionProvider>
+        </body>
+      </html>
   );
 }
