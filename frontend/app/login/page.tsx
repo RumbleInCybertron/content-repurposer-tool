4fc5fa9def30d8e0c6ac7eafe6c55e5b391d1de4
Derek <RumbleInCybertron@users.noreply.github.com>
2025-01-04 19:20:29 +0900

    + callback to login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -10,13 +10,13 @@ export default function LoginPage() {
       <div className="bg-white p-6 shadow-lg rounded-lg">
         <h1 className="text-2xl font-bold mb-4">Login</h1>
         <button
-          onClick={() => signIn("google")}
+          onClick={() => signIn("google", {callbackUrl: "/profile"})}
           className="bg-blue-500 text-white px-4 py-2 rounded w-full hover:bg-blue-700 mb-2"
         >
           Login with Google
         </button>
         <button
-          onClick={() => signIn("twitter")}
+          onClick={() => signIn("twitter", {callbackUrl: "/profile"})}
           className="bg-blue-400 text-white px-4 py-2 rounded w-full hover:bg-blue-600"
         >
           Login with Twitter
